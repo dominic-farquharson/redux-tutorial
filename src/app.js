aad8e401fabc235158a35e841ca2c02eeccc18f1
dominic-farquharson <farquharsondominic@gmail.com>
2017-08-12 17:26:53 -0400

    Added Redux store

--- src/app.js
+++ src/app.js
@@ -10,3 +10,37 @@ const counter = (state = 0, action) => {
     }
 }
 
+/*
+  Store: 
+    1. holds applications state object
+    2. dispatch actions
+*/
+const { createStore } = Redux;
+// specifying reducer to tell how state is updated based on actoin
+const store = createStore(counter); // counter will manage state updates
+
+// Store has three important methods
+
+/*
+  getState: returns state of application
+*/
+console.log(store.getState())
+
+/*
+  dispatch: dispatch actions to change state of applicatoin
+*/
+store.dispatch({ type: 'INCREMENT' }); // updating state
+console.log(store.getState())
+
+/*
+  Subscribe: let syou register callback that redux store will call
+  whenever an action has been dispatched
+    - can use to update UI of application whenever state has changed
+*/
+store.subscribe(() => {
+  document.body.innerText = store.getState();
+})
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' })
+})
