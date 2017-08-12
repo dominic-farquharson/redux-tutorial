5a382ce8fe73985712b4b948426341cf9c1e9e47
dominic-farquharson <farquharsondominic@gmail.com>
2017-08-12 17:34:08 -0400

    Able to render state to the DOM

--- src/app.js
+++ src/app.js
@@ -1,46 +1,53 @@
-// reducer
-const counter = (state = 0, action) => {
-    switch(action.type) {
-        case 'INCREMENT':
-          return state + 1;
-        case 'DECREMENT':
-          return state - 1;
-        default:
-          return state; 
-    }
-}
+document.addEventListener('DOMContentLoaded', function() {
+  // reducer
+  const counter = (state = 0, action) => {
+      switch(action.type) {
+          case 'INCREMENT':
+            return state + 1;
+          case 'DECREMENT':
+            return state - 1;
+          default:
+            return state; 
+      }
+  }
 
-/*
-  Store: 
-    1. holds applications state object
-    2. dispatch actions
-*/
-const { createStore } = Redux;
-// specifying reducer to tell how state is updated based on actoin
-const store = createStore(counter); // counter will manage state updates
+  /*
+    Store: 
+      1. holds applications state object
+      2. dispatch actions
+  */
+  const { createStore } = Redux;
+  // specifying reducer to tell how state is updated based on actoin
+  const store = createStore(counter); // counter will manage state updates
 
-// Store has three important methods
+  const render = () => {
+    document.body.innerText = store.getState();
+  }
 
-/*
-  getState: returns state of application
-*/
-console.log(store.getState())
+  render(); //rendering initial state
 
-/*
-  dispatch: dispatch actions to change state of applicatoin
-*/
-store.dispatch({ type: 'INCREMENT' }); // updating state
-console.log(store.getState())
+  // Store has three important methods
 
-/*
-  Subscribe: let syou register callback that redux store will call
-  whenever an action has been dispatched
-    - can use to update UI of application whenever state has changed
-*/
-store.subscribe(() => {
-  document.body.innerText = store.getState();
-})
+  /*
+    getState: returns state of application
+  */
+  console.log(store.getState())
 
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' })
+  /*
+    dispatch: dispatch actions to change state of application. 
+  */
+  store.dispatch({ type: 'INCREMENT' }); // updating state
+  console.log(store.getState())
+
+  /*
+    Subscribe: let syou register callback that redux store will call
+    whenever an action has been dispatched
+      - can use to update UI of application whenever state has changed
+  */
+  store.subscribe(render);
+
+  document.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREMENT' })
+  })
+
 })
